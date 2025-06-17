87de4d5a09ad9b3392bf72bc20136bc1d1ecd175
Brian Kamau <bkama099@uottawa.ca>
2025-06-17 18:53:30 -0400

    v1.1: created inital templates

--- ecommerce/store/views.py
+++ ecommerce/store/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
 
-# Create your views here.
+def store(request):
+    """
+    Render the store page.
+    """
+    context = {}
+    return render(request, 'store/store.html', context)
+
+def cart(request):
+    """
+    Render the cart page.
+    """
+    context = {}
+    return render(request, 'store/cart.html', context)
+
+def checkout(request):
+    """
+    Render the checkout page.
+    """
+    context = {}
+    return render(request, 'store/checkout.html', context)
